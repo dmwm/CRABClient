4a68c435a1c1d009d5eff908b9a7f91cc7f7018b
spiga <spiga@4525493e-7705-40b1-a816-d608a930855b>
2011-06-16 06:52:06 +0000

    From cafb307acfb8fd296a53c5d752b6628788648fc3 Mon Sep 17 00:00:00 2001 Subject: [PATCH] expand on status
    From: Eric Vaandering <ewv@fnal.gov>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/CRABClient/trunk@13046 4525493e-7705-40b1-a816-d608a930855b

--- src/python/Commands/status.py
+++ src/python/Commands/status.py
@@ -2,7 +2,6 @@ from Commands import CommandResult
 from Commands.SubCommand import SubCommand
 from ServerInteractions import HTTPRequests
 from client_utilities import loadCache
-import json
 
 
 class status(SubCommand):
@@ -35,10 +34,15 @@ class status(SubCommand):
         if status != 200:
             msg = "Problem retrieving status:\ninput:%s\noutput:%s\nreason:%s" % (str(cachedinfo['RequestName']), str(dictresult), str(reason))
             return CommandResult(1, msg)
- 
-        self.logger.info("Task Status:        %s"    % str(dictresult[unicode('RequestStatus')]))
-        self.logger.info("Completed at level: %s%% " % str(dictresult['percent_success']))
-    
+
+        self.logger.info("Task Status:        %s"    % str(dictresult['requestDetails'][unicode('RequestStatus')]))
+        self.logger.info("Completed at level: %s%% " % str(dictresult['requestDetails']['percent_success']))
+
+        for state in dictresult['states']:
+            count = dictresult['states'][state]['count']
+            jobList = readableRange(dictresult['states'][state]['jobs'])
+            self.logger.info("State: %s Count: %s  Jobs: %s" % (state, count, jobList))
+
         return CommandResult(0, None)
 
 
@@ -53,4 +57,35 @@ class status(SubCommand):
                                  default = None,
                                  help = "Same as -c/-continue" )
 
+    def readableRange(jobArray):
+        """
+        Take array of job numbers and concatenate 1,2,3 to 1-3
+        return string
+        """
+        def readableSubRange(subRange):
+            """
+            Return a string for each sub range
+            """
+            if len(subRange) == 1:
+                return "%s" % (subRange[0])
+            else:
+                return "%s-%s" % (subRange[0], subRange[len(subRange)-1])
 
+        # Sort the list and generate a structure like [[1], [4,5,6], [10], [12]]
+        jobArray.sort()
+
+        previous = jobArray[0]-1
+        listOfRanges = []
+        outputJobs = []
+        for job in jobArray:
+            if previous+1 == job:
+                outputJobs.append(job)
+            else:
+                listOfRanges.append(outputJobs)
+                outputJobs = [job]
+            previous = job
+        if outputJobs:
+            listOfRanges.append(outputJobs)
+
+        # Convert the structure to a readable string
+        return ','.join([readableSubRange(x) for x in listOfRanges])
