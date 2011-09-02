9109c3a4525159704776e770c3211a787fba30c7
spiga <spiga@4525493e-7705-40b1-a816-d608a930855b>
2011-09-02 13:46:53 +0000

    Added a check when .requestcache file is missing. Fixes #2254
    From: Marco Mascheroni <marco.mascheroni@cern.ch>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/CRABClient/trunk@13739 4525493e-7705-40b1-a816-d608a930855b

--- src/python/client_exceptions.py
+++ src/python/client_exceptions.py
@@ -5,5 +5,15 @@ class ClientException(Exception):
     pass
 
 class TaskNotFoundException(ClientException):
+    """
+    Raised when the task directory is not found
+    """
     ## defining the command line exit code associated with the exception
     exitcode = 4
+
+class CachefileNotFoundException(ClientException):
+    """
+    Raised when the .requestcache file is not found inside the Task directory
+    """
+    ## defining the command line exit code associated with the exception
+    exitcode = 5
