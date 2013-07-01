bc06a9457845337ea0beb17363bc82b688c08ac1
spiga <spiga@cern.ch>
2013-07-01 12:12:24 +0200

    update essential and full configs

--- doc/config/ExampleConfiguration.py
+++ doc/config/ExampleConfiguration.py
@@ -10,19 +10,18 @@ config = Configuration()
 ## General options for the client
 config.section_("General")
 config.General.requestName   = 'MyAnalysis_1'
-#config.General.serverUrl     = 'yourserver:0000'
-#config.General.instance = 'prod'
+#config.General.serverUrl     = 'yourserver'
+#config.General.instance = 'private'
 
 ## Specific option of the job type
 ## these options are directly readable from the job type plugin
 config.section_("JobType")
-config.JobType.pluginName  = 'Cmssw'
+config.JobType.pluginName  = 'Analysis'
 config.JobType.psetName    = 'pset.py'
 
 ## Specific data options
 config.section_("Data")
 config.Data.inputDataset = '/cms/data/set'
-#config.Data.processingVersion = 'v1'
 config.Data.splitting = 'LumiBased'
 config.Data.unitsPerJob = 20
 
