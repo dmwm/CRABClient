8aa24fa224f3509f55ad6b4341aad5215c0058a0
Hassen Riahi <Hassen.Riahi@cern.ch>
2013-07-09 22:54:40 +0200

    Add support of kill/resub of files.

--- src/python/CRABClient/Commands/kill.py
+++ src/python/CRABClient/Commands/kill.py
@@ -4,7 +4,6 @@ from CRABClient.client_exceptions import RESTCommunicationException
 
 import urllib
 
-
 class kill(SubCommand):
     """
     Simply call the server side of the kill
@@ -12,15 +11,39 @@ class kill(SubCommand):
     visible = True
 
     def __call__(self):
-        server = HTTPRequests(self.serverurl, self.proxyfilename)
+        if self.options.objects == 'jobs':
+            server = HTTPRequests(self.serverurl, self.proxyfilename)
+            self.logger.debug('Killing jobs of %s' % self.cachedinfo['RequestName'])
+            dictresult, status, reason = server.delete(self.uri, data = urllib.urlencode({ 'workflow' : self.cachedinfo['RequestName'], 'ASO': 0}))
+            self.logger.debug("Result: %s" % dictresult)
+            if status != 200:
+                msg = "Problem killing task %s:\ninput:%s\noutput:%s\nreason:%s" % \
+                        (self.cachedinfo['RequestName'], str(self.cachedinfo['RequestName']), str(dictresult), str(reason))
+                raise RESTCommunicationException(msg)
+            self.logger.info("Kill request succesfully sent")
+        if self.options.objects == 'files':
+            server = HTTPRequests(self.serverurl, self.proxyfilename)
+            self.logger.debug('Killing files of %s' % self.cachedinfo['RequestName'])
+            dictresult, status, reason = server.delete(self.uri, data = urllib.urlencode({ 'workflow' : self.cachedinfo['RequestName'], 'ASO':1 }))
+            self.logger.debug("Result: %s" % dictresult)
+            if status != 200:
+                msg = "Problem killing task %s:\ninput:%s\noutput:%s\nreason:%s" % \
+                       (self.cachedinfo['RequestName'], str(self.cachedinfo['RequestName']), str(dictresult), str(reason))
+                raise RESTCommunicationException(msg)
+            self.logger.info("Kill request succesfully sent")
 
-        self.logger.debug('Killing task %s' % self.cachedinfo['RequestName'])
-        dictresult, status, reason = server.delete(self.uri, data = urllib.urlencode({ 'workflow' : self.cachedinfo['RequestName']}))
-        self.logger.debug("Result: %s" % dictresult)
+    def setOptions(self):
+        """
+        __setOptions__
 
-        if status != 200:
-            msg = "Problem killing task %s:\ninput:%s\noutput:%s\nreason:%s" % \
-                    (self.cachedinfo['RequestName'], str(self.cachedinfo['RequestName']), str(dictresult), str(reason))
-            raise RESTCommunicationException(msg)
+        This allows to set specific command options
+        """
 
-        self.logger.info("Kill request succesfully sent")
+        self.parser.add_option( "-o", "--objects",
+                                 dest = "objects",
+                                 type   = 'str',
+                                 default = 'jobs',
+                                 help = "-o files to kill transfering files. Default -o jobs"
+                                 )
+
+
