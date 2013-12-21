8cb3f1b845d31ee674c456583de0eef1bbf99da6
Brian Bockelman <bbockelm@cse.unl.edu>
2013-12-21 08:34:46 -0600

    Allow users to specify task runtime parameters at submit and resubmit time.

--- src/python/CRABClient/Commands/resubmit.py
+++ src/python/CRABClient/Commands/resubmit.py
@@ -14,11 +14,16 @@ class resubmit(SubCommand):
     -t/--task option
     """
     def __call__(self):
-        ## retrieving output files location from the server
         server = HTTPRequests(self.serverurl, self.proxyfilename, self.proxyfilename, version=__version__)
 
         self.logger.debug('Requesting resubmission for failed jobs in task %s' % self.cachedinfo['RequestName'] )
-        #inputdict = { "TaskResubmit": "Analysis", "ForceResubmit" : force }
+        configreq = { 'workflow' : self.cachedinfo['RequestName']}
+
+        for attr in ['maxmemory', 'maxjobruntime', 'numcores', 'priority']:
+            val = getattr(self, attr, None)
+            if val:
+                configreq[attr] = val
+
         dictresult, status, reason = server.post(self.uri, data = urlencode({ 'workflow' : self.cachedinfo['RequestName']}) + \
                                                     self.sitewhitelist + self.siteblacklist + '&' + urlencode(self.jobids))
         self.logger.debug("Result: %s" % dictresult)
@@ -41,13 +46,41 @@ class resubmit(SubCommand):
                                  dest = 'siteblacklist',
                                  default = None,
                                  help = "Set the sites you want to blacklist during the resubmission." + \
-                                            "Comma separated list of cms sites (e.g.: T2_ES_CIEMAT,T2_IT_Rome[...])")
+                                            " Comma separated list of cms sites (e.g.: T2_ES_CIEMAT,T2_IT_Rome[...])")
 
         self.parser.add_option( "-w", "--whitelist",
                                  dest = 'sitewhitelist',
                                  default = None,
                                  help = "Set the sites you want to whitelist during the resubmission." + \
-                                            "Comma separated list of cms sites (e.g.: T2_ES_CIEMAT,T2_IT_Rome[...])")
+                                            " Comma separated list of cms sites (e.g.: T2_ES_CIEMAT,T2_IT_Rome[...])")
+
+        self.parser.add_option( None, "--memory",
+                                 dest = 'maxmemory',
+                                 default = None,
+                                 type = "int",
+                                 help = "Set the maximum memory used per job in this task." + \
+                                            " This is in units of MB (e.g.: 2000 for 2GB of RAM)")
+
+        self.parser.add_option( None, "--cores",
+                                 dest = 'numcores',
+                                 default = None,
+                                 type = "int",
+                                 help = "Set the number of cores used per job in this task." + \
+                                            " (e.g.: 1 for single-threaded applications)")
+
+        self.parser.add_option( "-p", "--priority",
+                                 dest = 'priority',
+                                 default = None,
+                                 type = "int",
+                                 help = "Set the priority of this task compared to other tasks you own; tasks default to 10." + \
+                                            " This does not improve your share compared to other users")
+
+        self.parser.add_option( None, "--wall",
+                                 dest = 'maxjobruntime',
+                                 default = None,
+                                 type = "int",
+                                 help = "Set the maximum time, in hours, jobs in this task are allowed to run." + \
+                                            " Default is 24 hours.")
 
         self.parser.add_option( '-i', '--jobids',
                                 dest = 'jobids',
@@ -88,3 +121,28 @@ class resubmit(SubCommand):
         self.jobids = ''
         if getattr(self.options, 'jobids', None):
             self.jobids = validateJobids(self.options.jobids)
+
+        # Sanity checks for task sizes.  Limits are purposely fairly generous to provide some level of future-proofing.
+        # The server may restrict further.
+        self.numcores = None
+        if self.options.numcores != None:
+            if self.options.numcores < 1 or self.options.numcores > 128:
+                raise ConfigException("The number of requested cores (%d) must be between 1 and 128." % (self.options.numcores))
+            self.numcores = str(self.options.numcores)
+
+        self.maxjobruntime = None
+        if self.options.maxjobruntime != None:
+            if self.options.maxjobruntime < 1 or self.options.maxjobruntime > 336:
+                raise ConfigException("The requested max job runtime (%d hours) must be between 1 and 336 hours." % self.options.maxjobruntime)
+            self.maxjobruntime = str(self.options.maxjobruntime)
+
+        self.maxmemory = None
+        if self.options.maxmemory != None:
+            if self.options.maxmemory < 30 or self.options.maxmemory > 1024*30:
+                raise ConfigException("The requested per-job memory (%d MB) must be between 30 and 30720 MB." % self.options.maxmemory)
+            self.maxmemory = str(self.options.maxmemory)
+
+        self.priority = None
+        if self.options.priority != None:
+            self.priority = str(self.options.priority)
+
