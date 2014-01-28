974c33c4ea72236b1ce9819ec42335f2989bf873
Brian Bockelman <bbockelm@cse.unl.edu>
2014-01-28 08:35:04 -0600

    Add some example config files to the client.

---
+++ doc/ExampleConfiguration.py
@@ -0,0 +1,33 @@
+"""
+This is an example of a minimal CRAB3 configuration file.
+"""
+
+from WMCore.Configuration import Configuration
+import os
+
+config = Configuration()
+
+## General options for the client
+config.section_("General")
+config.General.requestName   = 'MyAnalysis'
+config.General.instance = 'preprod'
+
+## Specific option of the job type
+## these options are directly readable from the job type plugin
+config.section_("JobType")
+config.JobType.pluginName  = 'Analysis'
+config.JobType.psetName    = 'pset.py'
+
+## Specific data options
+config.section_("Data")
+config.Data.inputDataset = '/GenericTTbar/HC-CMSSW_5_3_1_START53_V5-v1/GEN-SIM-RECO'
+config.Data.splitting = 'LumiBased'
+config.Data.unitsPerJob = 20
+
+## User options
+config.section_("User")
+config.User.email = ''
+
+config.section_("Site")
+config.Site.storageSite = 'T2_US_Nebraska'
+
