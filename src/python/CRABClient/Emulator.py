b1e741b39e8c7254f775eda3aced1e07bcdc1109
Andrew Melo <andrew.m.melo@vanderbilt.edu>
2014-08-22 14:53:47 -0500

    Lazy-load external dependencies

--- src/python/CRABClient/Emulator.py
+++ src/python/CRABClient/Emulator.py
@@ -2,10 +2,7 @@
     Used to perform dependency injection necessary for testing
 """
 
-import RESTInteractions
-from WMCore.Services.UserFileCache.UserFileCache import UserFileCache
-defaultDict = {'rest' : RESTInteractions.HTTPRequests,
-               'ufc' : UserFileCache}
+defaultDict = None
 overrideDict = {}
 
 def clearEmulators():
@@ -17,8 +14,17 @@ def getEmulator(name):
     if name in overrideDict:
         return overrideDict[name]
     else:
+        if not defaultDict:
+            defaultDict = getDefaults()
         return defaultDict[name]
 
 def setEmulator(name, value):
     global overrideDict
     overrideDict[name] = value
+
+def getDefaults():
+    import RESTInteractions
+    from WMCore.Services.UserFileCache.UserFileCache import UserFileCache
+    return {'rest' : RESTInteractions.HTTPRequests,
+                'ufc' : UserFileCache}
+
