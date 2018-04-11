058ad973eb10452a4fb13210859b702f33ecc17f
Leonardo Cristella <leonardo.cristella@cern.ch>
2018-04-11 09:02:45 +0200

    Adjust warnings thresholds

--- src/python/CRABClient/Commands/status.py
+++ src/python/CRABClient/Commands/status.py
@@ -465,14 +465,14 @@ class status(SubCommand):
         if mem_cnt or run_cnt:
             # Print a summary with memory/cpu usage.
             hint = " and/or improve the jobs splitting (e.g. config.Data.splitting = 'Automatic') in a new task"
-            usage = {'memory':[mem_max,maxMemory,0.7,parametersMapping['on-server']['maxmemory']['default'],'MB'], 'runtime':[to_hms(run_max),maxJobRuntime,0.3,parametersMapping['on-server']['maxjobruntime']['default'],'min']}
+            usage = {'memory':[mem_max,maxMemory,0.7,parametersMapping['on-server']['maxmemory']['default'],'MB'], 'runtime':[to_hms(run_max),maxJobRuntime,0.3,0,'min']}
             for param, values in usage.items():
                 if values[1] > values[3]:
                     if values[0] < values[2]*values[1]:
                         self.logger.info("\n%sWarning%s: the max jobs %s is less than %d%% of the task requested value (%d %s), please consider to request a lower value (allowed through crab resubmit)%s." % (colors.RED, colors.NORMAL, param, values[2]*100, values[1], values[4], hint))
             if run_sum:
                 cpu_ave = (cpu_sum / run_sum)
-                cpu_thr = 0.7
+                cpu_thr = 0.5
                 if cpu_ave < cpu_thr:
                     self.logger.info("\n%sWarning%s: the average jobs CPU efficiency is less than %d%%, please consider to request a lower number of threads%s." % (colors.RED, colors.NORMAL, cpu_thr*100, hint))
 
