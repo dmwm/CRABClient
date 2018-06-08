a8aaf8fb9397227490ca1911edfc8ad649813504
Leonardo Cristella <leonardo.cristella@cern.ch>
2018-06-08 19:39:51 +0200

    Improve message

--- src/python/CRABClient/Commands/submit.py
+++ src/python/CRABClient/Commands/submit.py
@@ -192,7 +192,7 @@ class submit(SubCommand):
 
         ## Check that --dryrun is not used with Automatic splitting
         if getattr(self.configuration.Data, 'splitting', 'Automatic') == 'Automatic' and self.options.dryrun: 
-            msg = "The 'dryrun' option is not needed with the 'Automatic' splitting mode (default)."
+            msg = "The 'dryrun' option is not compatible with the 'Automatic' splitting mode (default)."
             return False, msg
 
         ## Check that Data.unitsPerjob is specified.
