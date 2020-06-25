eaf493b0e839aeaa5bb29c128e5014be6809a326
belforte <stefano.belforte@cern.ch>
2020-06-25 15:24:04 -0500

    fix import of HTTPException for python3 compatibility

--- src/python/CRABClient/JobType/Analysis.py
+++ src/python/CRABClient/JobType/Analysis.py
@@ -10,7 +10,7 @@ import tempfile
 import uuid
 from functools import reduce
 
-from httplib import HTTPException
+from http.client import HTTPException
 
 from WMCore.DataStructs.LumiList import LumiList
 
