602f9823fd90f1d9a0e5fb88e930d5806db881f9
belforte <stefano.belforte@cern.ch>
2021-04-30 22:22:51 +0200

    add parasitic upload of tarablls to S3. Fix #4996

--- src/python/CRABClient/JobType/UserTarball.py
+++ src/python/CRABClient/JobType/UserTarball.py
@@ -6,11 +6,16 @@
 
 from  __future__ import division   # make division work like in python3
 
+import json
 import os
 import glob
 import math
+import time
+import socket
 import tarfile
 import tempfile
+import hashlib
+import uuid
 
 import CRABClient.Emulator
 from CRABClient.ClientMapping import configParametersInfo
@@ -22,6 +27,41 @@ from ServerUtilities import NEW_USER_SANDBOX_EXCLUSIONS, BOOTSTRAP_CFGFILE_DUMP
 from ServerUtilities import FILE_SIZE_LIMIT
 from ServerUtilities import uploadToS3
 
+def testS3upload(s3tester, archiveName, logger):
+    hasher = hashlib.sha256()
+    with open(archiveName) as f:
+        BUF_SIZE = 1024 * 1024  # lets read stuff in 1MByte chunks!
+        while True:
+            data = f.read(BUF_SIZE)
+            if not data:
+                break
+        hasher.update(data)
+    s3hash = hasher.hexdigest()
+    cachename = "%s.tgz" % s3hash
+    try:
+        t1 = time.time()
+        timestamp = time.strftime('%y%m%d_%H%M%S', time.gmtime())
+        msecs = int((t1 - int(t1)) * 1000)
+        timestamp += '.%03d' % msecs
+
+        uploadToS3(crabserver=s3tester, objecttype='sandbox', filepath=archiveName,
+                   tarballname=cachename, logger=logger)
+        status = 'OK'
+    except Exception as e:
+        self.logger.exception(str(e))
+        status = 'FAIL'
+        reason = str(e)
+    t2 = time.time()
+    s3report = {'status':status}
+    if status == 'FAIL':
+        s3report['reason'] = reason
+    thisSite = socket.gethostname()
+    tarballKB = os.stat(archiveName).st_size // 1024
+    s3report['timestamp'] = timestamp
+    s3report['origin'] = thisSite
+    s3report['KBytes'] = tarballKB
+    s3report['seconds'] = int(t2-t1)
+    return s3report
 
 
 class UserTarball(object):
@@ -35,7 +75,7 @@ class UserTarball(object):
             Also adds user specified files in the right place.
     """
 
-    def __init__(self, name=None, mode='w:bz2', config=None, logger=None, crabserver=None):
+    def __init__(self, name=None, mode='w:bz2', config=None, logger=None, crabserver=None, s3tester=None):
         self.config = config
         self.logger = logger
         self.scram = ScramEnvironment(logger=self.logger)
@@ -44,6 +84,7 @@ class UserTarball(object):
         self.checksum = None
         self.content = None
         self.crabserver = crabserver
+        self.s3tester = s3tester
 
     def addFiles(self, userFiles=None, cfgOutputName=None):
         """
@@ -204,11 +245,27 @@ class UserTarball(object):
         else:
             # old way using UFC
             ufc = CRABClient.Emulator.getEmulator('ufc')({'endpoint' : filecacheurl, "pycurl": True})
+            t1 = time.time()
             result = ufc.upload(archiveName, excludeList = NEW_USER_SANDBOX_EXCLUSIONS)
+            ufcSeconds = int(time.time()-t1)
             if 'hashkey' not in result:
                 self.logger.error("Failed to upload archive: %s" % str(result))
                 raise CachefileNotFoundException
             hashkey = str(result['hashkey'])
+            # upload a copy to S3 dev as well, just to stress it a bit, this never raises
+            s3report = testS3upload(self.s3tester, archiveName, self.logger)
+            # upload S3 test report to crabcache
+            # report also how log it tool uploading to UFC (which surely worked if we are here)
+            s3report['ufcseconds'] = ufcSeconds
+            reportFile = '/tmp/crabs3report.' + uuid.uuid4().hex
+            with open(reportFile,'w') as fp:
+                json.dump(s3report, fp)
+            reportName = s3report['timestamp'] + ':s3report.json'
+            try:
+                ufc.uploadLog(reportFile, reportName)
+            except Exception as e:
+                self.logger.exception(str(e))
+            os.remove(reportFile)
         return hashkey
 
 
