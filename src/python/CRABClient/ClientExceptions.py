80688c5b2a433047b57bc9fda19a3ab38639dafc
ddaina <daina.dirmaite@cern.ch>
2021-08-24 10:22:12 +0200

    fix curl error reporting (#5067)

--- src/python/CRABClient/ClientExceptions.py
+++ src/python/CRABClient/ClientExceptions.py
@@ -95,3 +95,8 @@ class PanDaException(ClientException):
     """
     exitcode = 3100
 
+class RestDataProcessingException(ClientException):
+    """
+    Errors coming from interaction with REST interface
+    """
+    exitcode = 3016
