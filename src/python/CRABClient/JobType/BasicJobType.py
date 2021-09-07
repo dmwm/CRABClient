f8ee92939ac62236e0f45e1ff990758b7b3a7787
Stefano Belforte <stefano.belforte@cern.ch>
2021-09-07 22:28:30 +0200

    take care run # as a string of bytes in report2 output. Fix #5080 (#5081)
    * take care run # as a string of bytes in report2 output. Fix #5080
    
    * pylint

--- src/python/CRABClient/JobType/BasicJobType.py
+++ src/python/CRABClient/JobType/BasicJobType.py
@@ -28,7 +28,7 @@ class BasicJobType(object):
         if config:
             valid, msg = self.validateConfig(config)
             if valid:
-                self.config  = config
+                self.config = config
                 self.workdir = workingdir
             else:
                 msg += "\nThe documentation about the CRAB configuration file can be found in"
@@ -65,8 +65,10 @@ class BasicJobType(object):
         for reports in inputdata.values():
             for report in reports:
                 for run, lumis in literal_eval(report['runlumi']).items():
+                    if isinstance(run, bytes):
+                        run = run.decode(encoding='UTF-8')
                     for lumi in lumis:
-                        mergedLumis.add((run,int(lumi))) #lumi is str, but need int
+                        mergedLumis.add((run, int(lumi))) #lumi is str, but need int
         mergedLumis = LumiList(lumis=mergedLumis)
         return mergedLumis.getCompactList()
 
