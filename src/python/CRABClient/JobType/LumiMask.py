2ce17f5a96b52bd4f4600372d9d4ef2edb60055e
Daina <60326190+ddaina@users.noreply.github.com>
2021-10-28 15:22:41 +0200

    remove unused import

--- src/python/CRABClient/JobType/LumiMask.py
+++ src/python/CRABClient/JobType/LumiMask.py
@@ -7,7 +7,6 @@ if sys.version_info >= (3, 0):
 if sys.version_info < (3, 0):
     from urlparse import urlparse
 
-from WMCore.Lexicon import jobrange
 from WMCore.DataStructs.LumiList import LumiList
 
 from CRABClient.ClientExceptions import ConfigurationException
