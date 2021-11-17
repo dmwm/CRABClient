f44a82d04e76737a32f8a0e60e48abf5780c4e55
Stefano Belforte <stefano.belforte@cern.ch>
2021-11-17 16:40:18 +0100

    Create WMCoreConfigWrapper.py

--- src/python/CRABClient/WMCoreConfigWrapper.py
+++ src/python/CRABClient/WMCoreConfigWrapper.py
@@ -1,20 +1,20 @@
 """
 This is a temporary file used to help transitioning to a
 CRABClient which has no dependency on WMCore. Its only purpose
-is to allow users to keep having this line in crabConfig.py
+is to allow users to keep having this line in crabConfig.py:
 from WMCore.Configuration import Configuration
 instead of :
 from CRABClient.Configuration import Configuration
 
 File is meant to be renamed and moved by the CRABClient build procedure  
-(crab-build.file in cmsdist or an ad hoc setup script in case a developer wants
-to run using source from GH) so that python discovers it as
-WMCore.Configuration. When executed, it prints a simple warning
+(crab-build.file in cmsdist)or an ad hoc setup script in case a developer wants
+to run using source from GH, so that python discovers it as
+WMCore.Configuration. When executed, the code oi here prints a simple warning
 asking the user to update the configuration and calls
 CRABClient.Configuration. In CRABClient.Configuration we have
 a modified (and frozen) version of WMCore.Configuration which
 runs in both python2 and python3 without requiring external
-dependencies which are not available in CMSSW_8 or earlies, e.g. "future"
+dependencies which are not available in CMSSW_8 or earlier, e.g. "future"
 """
 
 from CRABClient.Configuration import Configuration as Config
@@ -26,8 +26,11 @@ logger = logging.getLogger("CRAB3.all")
 class Configuration(Config):
     def __init__(self):
         msg = ''
-        msg += '%sWarning: Please update your config file to use configuration from CRABClient instead of WMCore.\n' % (colors.RED)
-        msg += 'Change it from "from WMCore.Configuration import Configuration" to "from CRABClient.Configuration import Configuration%s"' % (colors.NORMAL)
+        msg += '%sWarning: CRABClient does not depend on WMCore anymore.\n' % (colors.RED)
+        msg += 'Please update your config file to use configuration from CRABClient instead.\n'
+        msg += 'Change inside that file it from "from WMCore.Configuration import Configuration"\n'
+        msg += 'to "from CRABClient.Configuration import Configuration%s\n"' % (colors.NORMAL)
+        msg += 'Support for old style "from WMCore.Configuration import ..." will be remove in future versions of CRAB'
         logger.info(msg)
         Config.__init__(self)
         
