2ceaca1351b0cc158efc6cb84ae26d174b465330
Stefano Belforte <stefano.belforte@cern.ch>
2023-10-19 22:48:16 +0200

    crab status reports Rucio quota and alarms if needed. Fix #5206 (#5236)

--- src/python/CRABClient/Commands/status.py
+++ src/python/CRABClient/Commands/status.py
@@ -20,6 +20,7 @@ if sys.version_info < (3, 0):
 
 from CRABClient.ClientUtilities import colors, validateJobids, compareJobids
 from CRABClient.ClientUtilities import PKL_R_MODE
+from CRABClient.ClientUtilities import RUCIO_QUOTA_WARNING_GB, RUCIO_QUOTA_MINIMUM_GB
 from CRABClient.UserUtilities import curlGetFileFromURL, getColumn
 from CRABClient.Commands.SubCommand import SubCommand
 from CRABClient.ClientExceptions import ConfigurationException
@@ -66,6 +67,7 @@ class status(SubCommand):
         numCores = int(getColumn(crabDBInfo, 'tm_numcores'))
         splitting = getColumn(crabDBInfo, 'tm_split_algo')
         outputLfn = getColumn(crabDBInfo, 'tm_output_lfn')
+        outputDestinationSite = getColumn(crabDBInfo, 'tm_asyncdest')
         if getColumn(crabDBInfo, 'tm_output_dataset'):
             outputDatasetList = literal_eval(getColumn(crabDBInfo, 'tm_output_dataset'))
         else:
@@ -234,6 +236,10 @@ class status(SubCommand):
         combinedStatus = dagStatus = self.printDAGStatus(dbStatus, statusCacheInfo)
 
         usingRucio = outputLfn.startswith('/store/user/rucio') or outputLfn.startswith('/store/group/rucio')
+
+        if dagStatus != 'COMPLETED' and usingRucio:
+            self.printRucioQuotaInfo(user, outputDestinationSite)
+
         container = None
         if usingRucio:
             containerInfo = {
@@ -1128,6 +1134,34 @@ class status(SubCommand):
 
         return pubStatus
 
+    def printRucioQuotaInfo(self, user, site):
+        if not self.rucio:
+            return
+        if site == 'T3_CERN_CERNBOX':
+            return
+        # check Rucio quota and warn user as proper
+        try:
+            # Rucio returns a generator, so convert to a list and pick the only element (one site!)
+            quota = list(self.rucio.get_local_account_usage(user, site))[0]
+        except Exception as e:
+            print("Warning: Failed to get Rucio quota info\n %s" %e)
+            return
+        freeGB = quota['bytes_remaining'] / 1000 / 1000 / 1000
+        totalGB = quota['bytes_limit'] / 1000 / 1000 / 1000
+        usedGB = quota['bytes'] / 1000 / 1000 / 1000
+        print("You have %d/%d GBytes available as Rucio quota at site %s" % (freeGB, totalGB, site))
+        if freeGB < RUCIO_QUOTA_MINIMUM_GB:
+            msg = "%sALARM: Not enough space at ASO destination %s" % (colors.RED, colors.NORMAL)
+            msg += "\n This very dangerous. Your output stageout will get stuck."
+            msg += "\n status information will be stale and could be misleading"
+            msg += "\n %sYOU MUST CLEANUP YOUR RUCIO SPACE IMMEDIATELY (remove some rules)%s:" % (colors.RED, colors.NORMAL)
+            self.logger.warning(msg)
+            return
+        if freeGB < RUCIO_QUOTA_WARNING_GB:
+            msg = "%sWarning%s: " % (colors.RED, colors.NORMAL)
+            msg += "This may be not enough for your output, leading to problems. Consider cleaning up"
+            self.logger.warning(msg)
+        return
 
     def setOptions(self):
         """
