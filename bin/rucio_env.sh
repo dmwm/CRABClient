0bc1ad01ad739a1c6219a40c0cbd4882bc1ed3f4
Stefano Belforte <stefano.belforte@cern.ch>
2023-10-26 11:26:57 +0200

    avoid rucio setup getting confused by existing CMSSW stuff (#5242)

--- bin/rucio_env.sh
+++ bin/rucio_env.sh
@@ -2,5 +2,6 @@
 # returns the two env. variables needed to make Rucio client work
 # on the current OS
 unset PYTHONPATH
+eval `scram unsetenv -sh`
 source /cvmfs/cms.cern.ch/rucio/setup-py3.sh > /dev/null
 echo $RUCIO_HOME $PYTHONPATH
