818aeb5006304787a6095ccffafa0fedd36c24da
Stefano Belforte <stefano.belforte@cern.ch>
2023-12-04 11:04:04 +0100

    add DBS info

--- src/python/CRABClient/Commands/checkdataset.py
+++ src/python/CRABClient/Commands/checkdataset.py
@@ -7,12 +7,19 @@ check disk availability of a dataset
 
 from __future__ import print_function, division
 
+import sys
 import json
 
 from CRABClient.Commands.SubCommand import SubCommand
 from CRABClient.ClientUtilities import execute_command, colors
 from CRABClient.ClientUtilities import commandUsedInsideCrab
-from CRABClient.ClientExceptions import MissingOptionException
+from CRABClient.ClientExceptions import MissingOptionException, ConfigurationException
+from CRABClient.RestInterfaces import getDbsREST
+
+if sys.version_info >= (3, 0):
+    from urllib.parse import urlencode
+if sys.version_info < (3, 0):
+    from urllib import urlencode
 
 class checkdataset(SubCommand):
     """
@@ -25,15 +32,20 @@ class checkdataset(SubCommand):
         SubCommand.__init__(self, logger, cmdargs)
         self.dataset = None
         self.interactive = False
-
+        self.dbsInstance = None
 
     def __call__(self):
 
         self.dataset = self.options.dataset
+        self.dbsInstance = self.options.dbsInstance
         self.interactive = commandUsedInsideCrab()
 
+        status, size = self.checkDatasetInDBS()
+        self.logger.info("dataset status in DBS is: %s", status)
+        self.logger.info("dataset size is: %s", size)
+
         if not self.rucio:
-            self.logger.error("Rucio client not available with this CMSSW version")
+            self.logger.error("Rucio client not available with this CMSSW version. Can't check disk availability")
             return {'commandStatus': 'FAILED'}
 
         containerScope, containerName, blocks = self.getInputs()
@@ -74,6 +86,50 @@ class checkdataset(SubCommand):
             self.logger.info("NONE")
 
         return {'commandStatus': 'SUCCESS'}
+
+    def checkDatasetInDBS(self):
+        """ return status and size (including unit) of the dataset as strings """
+
+        status = 'NotFound'
+        size = 0
+
+        dbsReader, _ = getDbsREST(instance=self.dbsInstance, logger=self.logger,
+                                  cert=self.proxyfilename, key=self.proxyfilename)
+        datasetName = self.dataset.split(':')[-1]
+        isBlock = datasetName if  '#' in datasetName else None
+        if not isBlock:
+            query = {'dataset': datasetName, 'dataset_access_type': '*', 'detail': True}
+            ds, rc, msg = dbsReader.get(uri="datasets", data=urlencode(query))
+            if not ds:
+                self.logger.error("ERROR: %s not found in DBS", datasetName)
+                return status, size
+        else:
+            query =  {'block_name': datasetName}
+            bs, rc, msg = dbsReader.get(uri="blocks", data=urlencode(query))
+            if not bs:
+                self.logger.error("ERROR: %s not found in DBS", datasetName)
+                return status, size
+        self.logger.debug('exitcode= %s', rc)
+        if rc != 200:  # this is HTTP code. 200=OK
+            self.logger.error("Error trying to talk with DBS:\n%s", msg)
+            return status, size
+        # OK, dataset/block found in DBS
+        if isBlock:
+            status = 'EXISTS'  # only datasets and files have status in DBS
+            query = {'block_name': datasetName}
+        else:
+            status = ds[0]['dataset_access_type']
+            query = {'dataset': datasetName}
+        fs, rc, msg = dbsReader.get(uri='filesummaries', data=urlencode(query))
+        byteSize = fs[0]['file_size']
+        gBytes = byteSize / 1000. / 1000.
+        if gBytes > 10000:
+            size = "%1.f TB" % (gBytes / 1000.)
+        else:
+            size = "%s GB" % int(gBytes)
+
+        return status, size
+
 
     def getInputs(self):
         """
@@ -221,6 +277,10 @@ class checkdataset(SubCommand):
                                dest='dataset',
                                default=None,
                                help='dataset of block ID or Rucio DID (scope:name)')
+        self.parser.add_option('--dbs-instance', dest='dbsInstance', default='prod/global',
+                               help="DBS instance. e.g. prod/global (default) or prod/phys03 or full URL."
+                                    + "\nUse at your own risk only if you really know what you are doing"
+                               )
 
     def validateOptions(self):
         SubCommand.validateOptions(self)
@@ -232,3 +292,8 @@ class checkdataset(SubCommand):
             ex = MissingOptionException(msg)
             ex.missingOption = "dataset"
             raise ex
+        dbsInstance = self.options.dbsInstance
+        if '/' not in dbsInstance or len(dbsInstance.split('/')) > 2 and not dbsInstance.startswith('https://'):
+            msg = "Bad DBS instance value %s. " % dbsInstance  # pylint: disable=consider-using-f-string
+            msg += "Use either server/db format or full URL"
+            raise ConfigurationException(msg)
